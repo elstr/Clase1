af9480f38dff79fcb90b796df242b7854a83001f
elstr <eleonora.lester@gmail.com>
2019-01-15 20:23:27 -0300

    (enh): Ejemplo buttons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
-import List from "./components/list2";
+import List from "./components/list";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      flag: true,
       todoItems: [{ id: 0, todo: "Comprar coca", categoria: "turbio" }]
     };
-    this.addItem = this.addItem.bind(this);
+    // this.addItem = this.addItem.bind(this);
+    this.llegueAlBoton = this.llegueAlBoton.bind(this);
   }
-  addItem() {}
+  // addItem() {}
+
+  llegueAlBoton() {
+    console.log("Llegue al boton");
+    console.log("This -", this);
+    this.setState({ flag: false });
+  }
+
   render() {
     return (
       <div className="App">
         <input type="text" />
-        <button onClick={this.addItem}>Add</button>
-        <List todoItems={this.state.todoItems} />
+
+        <button onClick={this.llegueAlBoton}>PRIMER BOTON</button>
+        <button onClick={() => this.llegueAlBoton2()}>SEGUNDO BOTON</button>
+
+        {/* <button onClick={this.addItem.}>Add</button>
+        <List todoItems={this.state.todoItems} /> */}
       </div>
     );
   }
